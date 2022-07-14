538d312147ff8fadb3f40fab3d34e9b80cd8d096
Kirill <klarsky.kirill6@gmail.com>
2022-07-14 20:29:09 +0200

    in progress

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args)  {
-        String path = "C:\\Users\\Kirill\\IdeaProjects\\TestAufgabeMaven\\src\\main\\resources\\input.txt";
+        String path = "C:\\Users\\Kirill\\IdeaProjects\\TestAufgabeMaven\\src\\main\\resources\\inputTest.txt";
         ArrayList<Linie> linien = getLinesFromFile(path);
 
         ArrayList<Linienzug> linienZuege = converLinienToLinienzuege(linien);
@@ -18,29 +18,50 @@ public class Main {
         ArrayList<Linienzug> linienZuege = new ArrayList<>();
 
 
-       for (int i = 0; i<linien.size();i++){
-           //init
-           Linienzug linienZug = new Linienzug();
-           linienZug.addLine(linien.get(i));
+//       for (int i = 0; i<linien.size();i++){
+//           //init
+//           Linienzug linienZug = new Linienzug();
+//           linienZug.addLine(linien.get(i));
+//
+//           for (int j = 0; j<linien.size();j++){
+//               //pass this
+//                if(linien.get(i).equals(linien.get(j))){
+//                    continue;
+//                }
+//
+//                if(
+//                        linien.get(i).getStartPoint().equals(linien.get(j).getStartPoint())
+//                        ||linien.get(i).getStartPoint().equals(linien.get(j).getEndPoint())
+//                        ||linien.get(i).getEndPoint().equals(linien.get(j).getStartPoint())
+//                        ||linien.get(i).getEndPoint().equals(linien.get(j).getEndPoint())
+//                ){
+//                    linienZug.addLine(linien.get(j));
+//                    break;
+//                }
+//
+//           }
+//       }
 
-           for (int j = 0; j<linien.size();j++){
-               //pass this
-                if(linien.get(i).equals(linien.get(j))){
+        for (Linie mainLine:linien){
+            Linienzug linienZug = new Linienzug();
+            linienZug.addLine(mainLine);
+
+            linien.removeAll(linienZug.getLinien());
+            for (Linie lineToAdd:linien){
+                if(mainLine.equals(lineToAdd)){
                     continue;
                 }
 
                 if(
-                        linien.get(i).getStartPoint().equals(linien.get(j).getStartPoint())
-                        ||linien.get(i).getStartPoint().equals(linien.get(j).getEndPoint())
-                        ||linien.get(i).getEndPoint().equals(linien.get(j).getStartPoint())
-                        ||linien.get(i).getEndPoint().equals(linien.get(j).getEndPoint())
+                        mainLine.getStartPoint().equals(lineToAdd.getStartPoint())
+                        ||mainLine.getStartPoint().equals(lineToAdd.getEndPoint())
+                        ||mainLine.getEndPoint().equals(lineToAdd.getStartPoint())
+                        ||mainLine.getEndPoint().equals(lineToAdd.getEndPoint())
                 ){
-                    linienZug.addLine(linien.get(j));
-                    break;
+                    linienZug.addLine(lineToAdd);
                 }
-
-           }
-       }
+            }
+        }
 
 
         return linienZuege;
